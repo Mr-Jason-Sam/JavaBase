cdc3596a0f01ccdf2694dc4bcde33bfea79fd426
JasonSam <mr.jason_sam@icloud.com>
2019-06-06 12:46:00 +0800

    binSort

--- src/Sort/BinSort.java
+++ src/Sort/BinSort.java
@@ -1,5 +1,30 @@
 package Sort;
 
-public class BinSort {
+import java.util.Arrays;
 
+public class BinSort {
+	public static void binSort(int[] data){
+	    int head,tail,mid,temp;
+	    for (int i = 1; i < data.length; i++) {
+	        temp = data[i];
+	        head = 0;
+	        tail = i - 1;
+	        while(head <= tail) {
+	            mid = (head+tail) >>> 1;
+	            if (data[mid] > temp)
+	                tail = mid - 1;
+	            else
+	                head = mid + 1;
+	        }
+	        for (int j = i -1; j >= head; j--)
+	        	data[j+1] = data[j];
+	        data[head]= temp;
+	    }
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {10, 6, 3, 5, 7, 8, 2, 1, 4, 9};
+		binSort(array);
+		System.out.println(Arrays.toString(array));
+	}
 }
